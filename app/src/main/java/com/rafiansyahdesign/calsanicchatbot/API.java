46e8af998d4fe4f8c71e6a1745035a878f603c29
Mahesa Rafian Syah <mahesarafiansyah@gmail.com>
2023-07-21 06:01:34 +0700

    Update

--- app/src/main/java/com/rafiansyahdesign/calsanicchatbot/API.java
+++ app/src/main/java/com/rafiansyahdesign/calsanicchatbot/API.java
@@ -2,5 +2,5 @@ package com.rafiansyahdesign.calsanicchatbot;
 
 public class API {
     public static String API_URL = "https://api.openai.com/v1/completions";
-    public static String API = "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";
+    public static String API = "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";
 }
